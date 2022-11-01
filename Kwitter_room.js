dfebfd10e1e2155707fd1aec9de8ab7f1215e5d2
Apxcal <110207049+Apxcal@users.noreply.github.com>
2022-11-01 15:53:07 -0700

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+var { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBzmMpLuc1RyhB6N-AJNi8KztlFGQdpsMA",
+  authDomain: "chatwebapp-5841b.firebaseapp.com",
+  projectId: "chatwebapp-5841b",
+  storageBucket: "chatwebapp-5841b.appspot.com",
+  messagingSenderId: "212183374017",
+  appId: "1:212183374017:web:04f5f0e4da983b7cfd9c40"
+};
+firebase.initializeApp(firebaseConfig);
